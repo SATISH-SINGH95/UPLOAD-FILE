a6dd3b7e6482e212eb6240cd62434a9e5153cccc
satish <sk95satish@gmail.com>
2024-01-16 13:31:05 +0530

    Constant file is added in which list of extensions are added

---
+++ src/main/java/com/uploadFile/constants.java
@@ -0,0 +1,10 @@
+package com.uploadFile;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class constants {
+
+    public static final List<String> EXTENSIONS = Arrays.asList(".doc", ".docx", ".png", ".jpeg", ".jpg", ".pdf", ".txt");
+    
+}
